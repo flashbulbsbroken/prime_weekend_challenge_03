1006e8ceaba1f425d7b21f613a9f027ac675ec91
Sarah Marrone <smarrone06@gmail.com>
2015-09-27 00:36:30 -0500

    test

--- client/app.js
+++ client/app.js
@@ -7,15 +7,17 @@ $(function() {
 
         for (var i = 0; i < students.length; i++) {
 
-            var active = "";
-
-            if(i == 0) {
-                i = active;
-            };
+            //var active = "";
+            //
+            //if(i == 0) {
+            //    i = active;
+            //};
 
                 var descriptionList = "";
 
-                    for (var iTwo = 0; i < students[i]['description'].length; iTwo++) {
+                var description = students[i].description;
+
+                    for (var iTwo = 0; i < description[iTwo].length; iTwo++) {
 
                     var list = $("<li class='bulletList'>" + students[i]['description'][iTwo] + "</li>");
 
